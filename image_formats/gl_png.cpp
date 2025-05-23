e6db31fc705bc86e0ae81476976f8f3574bf60be
injeckt <p1o1l1i1e1x1o1v@mail.ru>
2025-05-23 10:05:28 +0500

    added working png generator

--- image_formats/gl_png.cpp
+++ image_formats/gl_png.cpp
@@ -22,9 +22,12 @@ png_t::png_t(std::ofstream* file, unsigned width, unsigned height)
 }
 
 void png_t::generate(const std::vector<color_t> &image_buffer, color_t background) {
-    unsigned long crc = 0xffffffffUL;
+    for (size_t i = 0; i < m_identifier.size(); i++) {
+        *m_file << m_identifier[i];
+    }
 
     // IHDR
+    unsigned long crc = 0xffffffffUL;
     unsigned char ihdr_bytes[] = {
         // Data length
         0x00, 0x00, 0x00, 0x0d,
@@ -34,7 +37,7 @@ void png_t::generate(const std::vector<color_t> &image_buffer, color_t backgroun
         0x00, 0x00, 0x00, 0x01, // Width
         0x00, 0x00, 0x00, 0x01, // Height
         0x08, // Bit Depth
-        0x06, // Color Type
+        0x02, // Color Type
         0x00, // Compression Method
         0x00, // Filter Method
         0x00, // Interlace Method
@@ -42,26 +45,42 @@ void png_t::generate(const std::vector<color_t> &image_buffer, color_t backgroun
     const size_t ihdr_size = sizeof(ihdr_bytes);
     for (size_t i = 0; i < ihdr_size; i++) {
         *m_file << ihdr_bytes[i];
-        math_tools::crc32_fast(ihdr_bytes + i, 1, crc);
-        crc ^= 0xffffffffUL;
+        if (i > 3) {
+            crc = math_tools::crc32_fast(ihdr_bytes + i, 1, crc);
+            crc ^= 0xffffffff;
+        }
     }
-    crc ^= 0xffffffffUL;
-    *m_file << math_tools::htob32(crc);
+    crc ^= 0xffffffff;
+        unsigned char crc_0 = (crc & 0xff000000) >> 24;
+    unsigned char crc_1 = (crc & 0xff0000) >> 16;
+    unsigned char crc_2 = (crc & 0xff00) >> 8;
+    unsigned char crc_3 = crc & 0xff;
+    *m_file << crc_0;
+    *m_file << crc_1;
+    *m_file << crc_2;
+    *m_file << crc_3;
 
     // IDAT
     crc = 0xffffffffUL;
     unsigned char ascii_idat[4] = { 0x49, 0x44, 0x41, 0x54 };
-    const size_t image_buffer_size = image_buffer.size();
-    *m_file << math_tools::htob32(image_buffer_size);
+    const size_t image_buffer_size = image_buffer.size() * 3;
+    unsigned char size_0 = (image_buffer_size & 0xff000000) >> 24;
+    unsigned char size_1 = (image_buffer_size & 0xff0000) >> 16;
+    unsigned char size_2 = (image_buffer_size & 0xff00) >> 8;
+    unsigned char size_3 = image_buffer_size & 0xff;
+    *m_file << size_0;
+    *m_file << size_1;
+    *m_file << size_2;
+    *m_file << size_3;
     for (size_t i = 0; i < 4; i++) {
         *m_file << ascii_idat[i];
-        math_tools::crc32_fast(ascii_idat + i, 1, crc);
+        crc = math_tools::crc32_fast(ascii_idat + i, 1, crc);
         crc ^= 0xffffffffUL;
     }
-    for (size_t i = 0; i < image_buffer_size; ++i) {
+    for (size_t i = 0; i < image_buffer_size / 3; ++i) {
         auto alphaed = color_t::alpha_to_color(image_buffer[i], background);
-        *m_file << std::to_string(alphaed.r()) << ' ' << std::to_string(alphaed.g()) << ' ' 
-        << std::to_string(alphaed.b()) << std::endl;
+        *m_file << (unsigned char)alphaed.r() << (unsigned char)alphaed.g()  
+        << (unsigned char)(alphaed.b());
         crc = math_tools::crc32_fast(reinterpret_cast<unsigned char*>(&alphaed.r()), 1, crc);
         crc ^= 0xffffffffUL;
         crc = math_tools::crc32_fast(reinterpret_cast<unsigned char*>(&alphaed.g()), 1, crc);
@@ -70,19 +89,34 @@ void png_t::generate(const std::vector<color_t> &image_buffer, color_t backgroun
         crc ^= 0xffffffffUL;
     }
     crc ^= 0xffffffffUL;
-    *m_file << math_tools::htob32(crc);
+    crc_0 = (crc & 0xff000000) >> 24;
+    crc_1 = (crc & 0xff0000) >> 16;
+    crc_2 = (crc & 0xff00) >> 8;
+    crc_3 = crc & 0xff;
+    *m_file << crc_0;
+    *m_file << crc_1;
+    *m_file << crc_2;
+    *m_file << crc_3;
 
     // IEND
     crc = 0xffffffffUL;
-    *m_file << 0x00 << 0x00 << 0x00 << 0x00;
+    *m_file << (unsigned char)0x00 << (unsigned char)0x00 << 
+        (unsigned char)0x00 << (unsigned char)0x00;
     unsigned char ascii_iend[4] = { 0x49, 0x45, 0x4e, 0x44 };
     for (size_t i = 0; i < 4; i++) {
         *m_file << ascii_iend[i];
-        math_tools::crc32_fast(ascii_iend + i, 1, crc);
+        crc = math_tools::crc32_fast(ascii_iend + i, 1, crc);
         crc ^= 0xffffffffUL;
     }
     crc ^= 0xffffffffUL;
-    *m_file << math_tools::htob32(crc);
+    crc_0 = (crc & 0xff000000) >> 24;
+    crc_1 = (crc & 0xff0000) >> 16;
+    crc_2 = (crc & 0xff00) >> 8;
+    crc_3 = crc & 0xff;
+    *m_file << crc_0;
+    *m_file << crc_1;
+    *m_file << crc_2;
+    *m_file << crc_3;
 }
 
 std::string png_t::get_format_extension() {
--- image_formats/gl_png.hpp
+++ image_formats/gl_png.hpp
@@ -30,7 +30,7 @@ private:
     unsigned m_height;
 
 
-    const std::array<const int, 8> m_identifier = {
+    const std::array<unsigned char, 8> m_identifier = {
         0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A
     };
 
